8228c2c9650d334f25f7b7b8a83001dd1df76291
Davi Araujo <davi.araujo@onfly.com.br>
2023-12-02 22:30:21 -0300

    little adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <stdexcept>
 #include "segmentTree.hpp"
-using namespace std;
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     number segments, num_operations, target_segment, initial_instant = 0, final_instant = 0;
     auto p = Point(0, 0);
     cin >> segments >> num_operations;
@@ -16,13 +15,11 @@ int main(int argc, char* argv[]) {
     while (num_operations-- && cin >> operation) {
         if (operation == 'u') {
             auto matrix = Matrix();
-            cin >>  target_segment >> matrix;
+            cin >> target_segment >> matrix;
             tree.update(target_segment, matrix, 1, 0, segments - 1);
-        }
-        else if (operation == 'q') {
+        } else if (operation == 'q') {
             cin >> initial_instant >> final_instant >> p;
             cout << p * tree.query(initial_instant, final_instant, 1, 0, segments - 1) << endl;
-        }
-        else throw invalid_argument("Invalid operation " + operation);
+        } else throw invalid_argument("Invalid operation " + operation);
     }
 }
